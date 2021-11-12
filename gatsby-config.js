a599e3da8818fdd00f5cab1b3f5fe56ec385de41
flushprob <72862843+flushprob@users.noreply.github.com>
2021-11-12 18:37:20 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,75 +1,64 @@
 module.exports = {
   siteMetadata: {
     // Site URL for when it goes live
-    siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
+    siteUrl: `https://flushprob.github.io`,
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: '정인식 | Inshik Chung',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `Inshik Chung | Newbie Developer`,
     // Description that goes under your name in main bio
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
+    description: `Trying it`,
     // Optional: Twitter account handle
-    author: `@rfitzio`,
+    // author: `@rfitzio`,
     // Optional: Github account URL
-    github: `https://github.com/ryanfitzgerald`,
+    github: `https://github.com/flushprob`,
     // Optional: LinkedIn account URL
-    linkedin: `https://www.linkedin.com/in/ryanafitzgerald/`,
+    // linkedin: `https://www.linkedin.com/in/ryanafitzgerald/`,
     // Content of the About Me section
-    about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
+    about: `Currently studying Computer Engineering at Hanbat National University.`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
     projects: [
       {
-        name: 'Devfolio',
+        name: 'face blurring webApp',
         description:
-          'A zero-config and blazing fast personal site + blog built with GatsbyJs and TailwindCSS',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+          'Capstone Design 2021, Using caffe and mediapipe to blurring faces',
+        link: 'https://github.com/flushprob/faceblur_web',
       },
       {
-        name: 'ChromeExtensionKit',
+        name: 'zikgucal',
         description:
-          'Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates',
-        link: 'https://chromeextensionkit.com/?ref=devfolio',
+          'Simple Project 2020, Using Java to make simple caculator for expense of oversea shopping',
+        link: 'https://github.com/flushprob/zikgucal',
       },
       {
-        name: 'Another Cool Project',
+        name: 'Showerbooth for socially underprivileged',
         description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit ducimus perferendis',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+          'PROBONO ICT Project 2019, Keep safe while taking shower, if had accident, contact to guardian immediately',
+        link: 'https://github.com/flushprob/showerbooth_2019',
       },
     ],
     // Optional: List your experience, they must have `name` and `description`. `link` is optional.
     experience: [
       {
-        name: 'Acme Corp',
-        description: 'Full-Stack Developer, February 2020 - Present',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'Hanbat National University - Computer Engineering',
+        description: 'Student, March 2020 - February 2022',
       },
       {
-        name: 'Globex Corp',
-        description: 'Full-Stack Developer, December 2017 - February 2020',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
-      {
-        name: 'Hooli',
-        description: 'Full-Stack Developer, May 2015 - December 2017',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
+        name: 'Korea Polytechnic Daejeon - Information & Communication System',
+        description: 'Student, March 2016 - February 2020',
+      }
     ],
     // Optional: List your skills, they must have `name` and `description`.
     skills: [
       {
         name: 'Languages & Frameworks',
         description:
-          'JavaScript (ES6+), Golang, Node.js, Express.js, React, Ruby on Rails, PHP',
-      },
-      {
-        name: 'Databases',
-        description: 'MongoDB, PostreSQL, MySQL',
+          'C, Java, Python',
       },
       {
         name: 'Other',
         description:
-          'Docker, Amazon Web Services (AWS), CI / CD, Microservices, API design, Agile / Scrum',
+          'Git, English, Friendly, Hard working, Postive mind',
       },
     ],
   },
